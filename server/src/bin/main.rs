8533e74104e99947d7ff7e04a552e30a82a966b1
Alexander Andreev <al101an@mail.ru>
2019-01-18 06:11:38 +0700

    add server module, lib ThreadPool

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -27,6 +27,8 @@
 protector.py protector.py
 */
 
+use server::ThreadPool;
+
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
@@ -36,11 +38,12 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
