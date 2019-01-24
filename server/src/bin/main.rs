8af0bf82b3850e0f7db8513397aeffb652b41c01
Alexander Andreev <al101an@mail.ru>
2019-01-24 13:25:21 +0700

    parse CL arguments, fix *.html paths
    add clap dependency lib

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -27,18 +27,82 @@
 protector.py protector.py
 */
 
+extern crate clap;
+
+use clap::{Arg, App};
+
 use server::ThreadPool;
 
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use std::fs;
 use std::thread;
+use std::process;
 use std::time::Duration;
+use std::str::FromStr;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    /*
+     * Parse command line arguments
+     */
+    let matches = App::new("CSD-ProgLang-2018-4")
+        .about("Protected client/server application on rust")
+        .arg(Arg::with_name("[ip]:port")
+                 .takes_value(true)
+                 .required(true)
+                 .help("port - server mode, ip:port - client mode"))
+        .arg(Arg::with_name("connection_number")
+                 .short("n")
+                 .takes_value(true)
+                 .help("simultaneous connections number"))
+        .get_matches();
+
+    let ip_port_str = matches.value_of("[ip]:port").unwrap();
+    let ip_port: Vec<&str> = ip_port_str.split(":").collect();
+    if ip_port.len() == 1 { // server mode
+        let parsedPort = ip_port[0].parse::<i32>();
+        match parsedPort {
+            Ok(n) => println!("The port parsed is: {}", n),
+            Err(e) =>  { println!("Error parsing port: {:?}", e); process::exit(0); }
+        }
+
+        let num_str = matches.value_of("connection_number").unwrap_or("100");
+        let parsed_connections_number = num_str.parse::<usize>();
+        let mut connections_number: usize = 1;
+        match parsed_connections_number {
+            Ok(n) => { println!("simultaneous connections number {}.", n); connections_number = n; }
+            Err(e) => println!("That's not a number! {}", e),
+        }
+
+        // Start server
+        // for arguments use str variables, asserted by ip & i32 parsers
+        start_server(ip_port_str, connections_number);
+
+    } else if ip_port.len() == 2 { // client mode
+        let parsedIp = IpAddr::from_str(ip_port[0]);
+        match parsedIp {
+            Ok(p) => println!("The ip parsed is: {}", p),
+            Err(e) => { println!("Error parsing ip: {:?}", e); process::exit(0); }
+        }
+        let port = ip_port[1].parse::<i32>();
+        match port {
+            Ok(n) => println!("The port parsed is: {}", n),
+            Err(e) =>  { println!("Error parsing port: {:?}", e); process::exit(0); }
+        }
+
+        // Connect to server
+        connect_to_server(ip_port_str);
+    } else {
+        println!("Incorrect command-line arguments input!")
+    }
+}
+
+fn start_server(port: &str, connections_number: usize) {
+    let socket_addr = "127.0.0.1:".to_owned() + port;
+    let listener = TcpListener::bind(socket_addr).unwrap();
+    let pool = ThreadPool::new(connections_number);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -47,6 +111,10 @@ fn main() {
             handle_connection(stream);
         });
     }
+}
+
+fn connect_to_server(ip_port: &str) {
+    // TODO: implement method
 }
 
 fn handle_connection(mut stream: TcpStream) {
